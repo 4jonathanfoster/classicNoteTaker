a94d844993ae5518fea1b32a9cde0e79bf027908
4jonathanfoster <82682840+4jonathanfoster@users.noreply.github.com>
2021-08-09 19:08:04 -0500

    apinotes

--- server.js
+++ server.js
@@ -1,53 +1,39 @@
 const express = require(`express`);
-//const apiroutes = require(`./Routes/apiroutes`);
-//const htmlroutes = require(`./Routes/htmlroutes`);
+const { v4: uuidv4 } = require('uuid');
 const app = express();
 const path = require('path')
 const db = require('./db/db.json')
-
 const fs = require('fs')
 
-var bodyParser = require('body-parser')
-app.use( bodyParser.json() );       // to support JSON-encoded bodies
-app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
-  extended: true
-})); 
+const app = express();
+const PORT = 3000;
+const PORT = process.env.PORT || 3000 
 
-const PORT = process.env.PORT || 3000;
 
+app.use(express.static('public'));
 app.use(express.json());
-app.use(express.static("public"))
-//app.use("/api", apiroutes)
-//app.use("/", htmlroutes)
+app.use(express.urlencoded({ extended: true}));
 
-app.get('/notes',function(req,res){
-    console.log('we hit the route')
-    //res.send ('hello from the route')
-    res.sendFile(path.join(__dirname, './public','notes.html'));
+app.get(`/api/notes` ,(req, res) => {
+	console.log(`Notes Route`);
+	res.json(db);
 })
-console.log('dirname thing ?', __dirname)
 
-app.get('/api/notes',function(req,res){
-    console.log('we hit hte /api/notes route!!', db)
-    // fs.readFile('./db/db.json', 'utf8',function(err,data) {
-    //     console.log('data we read in', data, err)
-    //     res.json(JSON.parse(data))
-    // } )
-    res.json(db)  
-  //  res.sendFile(path.join(__dirname, './public','notes.html'));
+app.post(`/api/notes`, (req,res) => {
+	console.log(`new note to save `);
+
+	let newNote = req.body;
+	newNote.id = uuidv4();
+	db.push(newNote);
+	fs.writeFileSync("./db/db.json" , JSON.stringify(db), (err) => {
+	if(err) throw err;
+	});
+	res.send(db);
 })
 
 
-app.post('/api/notes', function(req, res){
-    console.log('New note to save!', req.body)
-
-    // take req.body and add it to the db thing we imported!!
-
-    // maybe a .push() !!
-
-})
 
 
 app.listen(PORT, () => {
-    console.log(`App listening on PORT: ${PORT}`);
+  console.log(`App listening on PORT: ${PORT}`);
 });
